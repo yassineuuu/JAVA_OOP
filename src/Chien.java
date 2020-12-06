41b564754807c30beb5aae702edd2ef5ae2345cb
yassineuuu <yassinemakhlouk49@gmail.com>
2020-12-06 00:02:30 +0000

    exercice1&2

---
+++ src/Chien.java
@@ -0,0 +1,40 @@
+public class Chien {
+    String nom;
+
+    public Chien(String nom) {
+        this.nom = nom;
+    }
+
+    //The Methods includs exercice 1 and 2 ;)
+
+    public void Aboyer(String sens){
+        if (sens.equalsIgnoreCase("Angry")){
+            System.out.print("Je suis en colère. Ghhhhhhhhhh !!! WWWWOARF !! WWWWOARF !!");
+        }else if (sens.equalsIgnoreCase("Cool")){
+            System.out.print("Je ne suis pas du tout en colère. Iwiw !! awaw !!");
+        }else {
+            System.out.print(nom + " dit wwaaaarf,");
+        }
+    }
+
+    public void Manger(String food){
+        if (food.equalsIgnoreCase("Fish")){
+            System.out.print(" Je mange du poisson");
+        }else {
+            System.out.println(" Je mange de la viande");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Chien Rex = new Chien("Rex");
+        Rex.Aboyer("Angry");
+        Rex.Manger("qlql");
+
+        Chien Max = new Chien("Max");
+        Max.Aboyer("cool");
+        Max.Manger("fish");
+    }
+
+}
+
